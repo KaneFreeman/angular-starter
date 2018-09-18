79a777182a51eaca07dcd0bbf5ade7e39ec1de17
Daniel Lautzenheiser <lautzd@gmail.com>
2018-09-18 13:00:33 -0400

    feat: update to angular-redux-util 3.0.0-alpha.2

--- src/app/reducers/example-redux.reducer.ts
+++ src/app/reducers/example-redux.reducer.ts
@@ -1,17 +1,15 @@
-import { assign } from 'angular-redux-util';
-
-import { AnyAction } from 'redux';
+import { assign, ReduxHttpSuccessAction } from 'angular-redux-util';
 
 import { ExampleReduxActions } from '../actions';
 import { ExampleReduxState, EXAMPLE_REDUX_INITIAL_STATE } from '../store';
 
-export function exampleReduxReducer(state: ExampleReduxState = EXAMPLE_REDUX_INITIAL_STATE, action: AnyAction) {
+export function exampleReduxReducer(state: ExampleReduxState = EXAMPLE_REDUX_INITIAL_STATE, action: ReduxHttpSuccessAction) {
   switch (action.type) {
     case ExampleReduxActions.INCREMENT_COUNTER:
       return assign(state, { exampleCounter: state.exampleCounter + 1 });
 
     case ExampleReduxActions.GET_DATA_SUCCESS:
-      return assign(state, { data: action.payload });
+      return assign(state, { data: action.response });
 
     default:
       return state;
